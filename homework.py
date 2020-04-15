64677507ea3759549dc30d0597ad57585288a016
yunchu99 <61811544+yunchu99@users.noreply.github.com>
2020-04-16 00:36:41 +0800

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,21 @@
+# 1.用input()函數取得姓名、年紀與體重的資訊
+## 用變數儲存資料
+# 2.用print()函數列印螢幕特定內容
+
+name=input("請問貴姓大名？")
+print("你好，",name)
+age=input("請問年紀？")
+print("原來你",age,"歲")
+weight=input("請問體重？")
+print("體重為",weight)
+
+print("我叫王韻筑")
+print("我今年19歲")
+print("我40公斤")
+
+name="王韻筑"
+print("我叫",name)
+age="19"
+print("我今年",age,"歲" )
+weight="40"
+print("我的體重",weight)
