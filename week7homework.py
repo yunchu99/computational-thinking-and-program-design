b887b00c90b68838b93ddd96cb4943bf532f2b6c
yunchu99 <61811544+yunchu99@users.noreply.github.com>
2020-04-22 21:45:06 +0800

    Add files via upload

---
+++ week7homework.py
@@ -0,0 +1,27 @@
+#1.從維基百科找出要記錄的梨泰院class韓劇相關資訊
+##網址:https://zh.wikipedia.org/wiki/%E6%A2%A8%E6%B3%B0%E9%99%A2Class
+##相關資訊:韓劇名稱、播出日期......
+
+#2.在Python用變數儲存資訊
+drama_title="Itaewon Class"#韓劇名稱
+release_year="2020"#播出日期
+drama_time_min=60#每集長度
+drama_rating=11.832#大韓民國AGB平均收視率
+
+director="Kim Sung Yoon"#導演
+stars_1="Park Seo Jun"#主要演員
+stars_2="Kim Da Mi"
+stars_3="Kwon A-yoon"
+
+like=7#你喜不喜歡
+
+#3.印出梨泰院class的相關資訊
+print(drama_title)#螢幕列印
+print(release_year)
+print(drama_time_min)
+print(drama_rating)
+print(director)
+print(stars_1)
+print(stars_2)
+print(stars_3)
+print(like)
